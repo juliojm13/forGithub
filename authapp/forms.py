7839c0bbd5204dec51a1bc6cd6365c7f922de2fb
Julio <juliojm1310@gmail.com>
2022-02-09 20:09:07 +0300

    add authapp

---
+++ authapp/forms.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserChangeForm
+from .models import ShopUser
+from django.forms import forms
+
+
+class ShopUserLoginForm(AuthenticationForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super(ShopUserLoginForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class ShopUserRegisterForm(UserCreationForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'first_name', 'password1', 'password2', 'email', 'city', 'avatar')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+
+
+class ShopUserEditForm(UserChangeForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'first_name', 'email','city', 'avatar', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+
